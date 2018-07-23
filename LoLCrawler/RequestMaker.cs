8bfc7eaddb670a5c2e5525711ff334f0f8911b63
James Hobday <jameshobday84@gmail.com>
2018-07-23 14:57:59 +0100

    Added RequestMaker to make httpRequests to a given url.

---
+++ LoLCrawler/RequestMaker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoLCrawler
+{
+    class RequestMaker
+    {
+        private async Task<string> makeRequest(string url)
+        {
+            HttpClient client = new HttpClient();
+            string result;
+
+            // Call asynchronous network methods in a try/catch block to handle exceptions
+            try
+            {
+                result = await client.GetStringAsync(url);
+
+            }
+            catch (HttpRequestException e)
+            {
+                result = e.Message;
+            }
+
+            // Need to call dispose on the HttpClient object
+            // when done using it, so the app doesn't leak resources
+            client.Dispose();
+            return result;
+        }
+
+        public string Fetch(string url)
+        {
+            return makeRequest(url).Result;
+        }
+    }
+}

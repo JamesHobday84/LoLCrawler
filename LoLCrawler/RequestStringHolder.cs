661dbb7f12d68f6cff5b8c09bf0f6a64f81638eb
James Hobday <jameshobday84@gmail.com>
2018-07-23 15:55:31 +0100

    refactored variable names in RequestStringHolder to reflect them being private variables as opposed to fields.

--- LoLCrawler/RequestStringHolder.cs
+++ LoLCrawler/RequestStringHolder.cs
@@ -6,24 +6,24 @@ namespace LoLCrawler
 {
     public static class RequestStringHolder
     {
-        private static string _euRoot = "https://euw1.api.riotgames.com/";
-        private static string _apiKey = "RGAPI-5e8ce71c-5bf6-4aff-b9ce-693da992c5de";
-        private static string _apiKeySuffix = $"api_key={_apiKey}";
-        private static string _summonerRequest = "lol/summoner/v3/summoners/by-name/"; //+name + ?;
-        private static string _matchListFromSummonerId = "lol/match/v3/matchlists/by-account/"; //+id + ?;
-        private static string _matchDetailedFromId = "lol/match/v3/matches/"; //+id + ?
+        private static string euRoot = "https://euw1.api.riotgames.com/";
+        private static string apiKey = "RGAPI-5e8ce71c-5bf6-4aff-b9ce-693da992c5de";
+        private static string apiKeySuffix = $"api_key={apiKey}";
+        private static string summonerRequest = "lol/summoner/v3/summoners/by-name/"; //+name + ?;
+        private static string matchListFromSummonerId = "lol/match/v3/matchlists/by-account/"; //+id + ?;
+        private static string matchDetailedFromId = "lol/match/v3/matches/"; //+id + ?
 
         public static string SummonerRequest(string name)
         {
-            return $"{_euRoot}{_summonerRequest}{name}?{_apiKeySuffix}";
+            return $"{euRoot}{summonerRequest}{name}?{apiKeySuffix}";
         }
         public static string MatchListRequest(string summonerId)
         {
-            return $"{_euRoot}{_matchListFromSummonerId}{summonerId}?{_apiKeySuffix}";
+            return $"{euRoot}{matchListFromSummonerId}{summonerId}?{apiKeySuffix}";
         }
         public static string _MatchDetailedRequest(string matchId)
         {
-            return $"{_euRoot}{_matchDetailedFromId}{matchId}?{_apiKeySuffix}";
+            return $"{euRoot}{matchDetailedFromId}{matchId}?{apiKeySuffix}";
         }
 
 

176066376c95cb57c20aabf7461ac0bb9b2422fc
James Hobday <jameshobday84@gmail.com>
2018-07-24 15:46:18 +0100

    Added new database entity LeagueDiv and created a new folder to hold entities. Name class moved to this folder too.

---
+++ LoLCrawler/DatabaseEntity/LeagueDiv.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoLCrawler.DatabaseEntity
+{
+    class LeagueDiv
+    {
+        public string summonerName { get; set; }
+        public string tier { get; set; }
+        public string rank { get; set; }
+    }
+}

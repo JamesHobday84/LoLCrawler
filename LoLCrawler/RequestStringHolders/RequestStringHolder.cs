c4558006b3ec1ed21cf378237d73af8901f6b91b
James Hobday <jameshobday84@gmail.com>
2018-07-25 19:39:46 +0100

    Extracted SummonerV3 api strings to tehir own class.

--- LoLCrawler/RequestStringHolders/RequestStringHolder.cs
+++ LoLCrawler/RequestStringHolders/RequestStringHolder.cs
@@ -6,6 +6,7 @@ namespace LoLCrawler
 {
     public static class RequestStringHolder
     {
+        
         //Shared strings for all api calls.
         private static string root = "https://euw1.api.riotgames.com/";
         private static string apiKey = "RGAPI-53e7278a-2924-4abb-8678-25ee3c7d6260";
---
+++ LoLCrawler/RequestStringHolders/SummonerV3StringHolder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoLCrawler.RequestStringHolders
+{
+    class SummonerV3StringHolder
+    {
+        public SummonerV3StringHolder(SharedStringHolder sharedStringHolder)
+        {
+            root = sharedStringHolder.Root();
+            apiKeySuffix = sharedStringHolder.ApiKeySuffix();
+        }
+
+        //Shared request strings.
+        string root;
+        string apiKeySuffix;
+
+        //Api specific request strings
+
+        private readonly string summonersByAccountId = "lol/summoner/v3/summoners/by-account/"; //+AccountId
+        private readonly string summonersByName = "lol/summoner/v3/summoners/by-name/"; //+SummonerName
+        private readonly string summonersBySummonerId = "lol/summoner/v3/summoners/"; //+SummonerId
+
+        //Assemble api request strings...
+
+        public string SummonersByAccountId(string accountId)
+        {
+            return $"{root}{summonersByAccountId}{accountId}?{apiKeySuffix}";
+        }
+        public string SummonersByName(string summonerName)
+        {
+            return $"{root}{summonersByName}{summonerName}?{apiKeySuffix}";
+        }
+        public string SummonerBySummonerId(string summonerId)
+        {
+            return $"{root}{summonersBySummonerId}{summonerId}?{apiKeySuffix}";
+        }
+    }
+}

7c2aa657c500aeafb457b9a70e906bb17205b2da
James Hobday <jameshobday84@gmail.com>
2018-07-25 18:42:00 +0100

    Added EuWestStringHolder to hold the shared data for all eu west api request strings.

---
+++ LoLCrawler/RequestStringHolders/ChampionMasteryV3StringHolder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoLCrawler.RequestStringHolders
+{
+    class ChampionMasteryV3StringHolder
+    {
+    }
+}
---
+++ LoLCrawler/RequestStringHolders/EuWestStringHolder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoLCrawler.RequestStringHolders
+{
+    static class EuWestStringHolder
+    {
+        private static string euRoot = "https://euw1.api.riotgames.com/";
+        private static string apiKey = "RGAPI-53e7278a-2924-4abb-8678-25ee3c7d6260";
+        private static string apiKeySuffix = $"api_key={apiKey}";
+
+        public static string EuRoot()
+        {
+            return euRoot;
+        }
+
+        public static string ApiKeySuffix()
+        {
+            return apiKeySuffix;
+        }
+    }
+}

539730d463a61804ce6b5abe7fe20041fd3799a2
James Hobday <jameshobday84@gmail.com>
2018-07-27 01:10:48 +0100

    Added tests for SpectatorV3StringHolder.

---
+++ LoLCrawlerTests/RequestStringHolderTests/SpectatorV3StringHolderTests.cs
@@ -0,0 +1,27 @@
+﻿using LoLCrawler;
+using LoLCrawler.RequestStringHolders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LoLCrawlerTests.RequestStringHolderTests
+{
+    public class SpectatorV3StringHolderTests
+    {
+        private readonly string apiKeySuffix = $"?api_key={ApiKeyHolder.ApiKey}";
+        private readonly string euRoot = "https://euw1.api.riotgames.com";
+
+        [Fact]
+        public void CorrectStringActiveGamesBySummonerId()
+        {
+            var sut = new SpectatorV3StringHolder(new EuWestStringHolder());
+            var summonerId = "10";
+            var expected = $"{euRoot}/lol/spectator/v3/active-games/by-summoner/{summonerId}{apiKeySuffix}";
+
+            var result = sut.ActiveGamesBySummonerId(summonerId);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}

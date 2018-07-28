534a5dd80f94fc474958e9517d366a264572bcce
James Hobday <jameshobday84@gmail.com>
2018-07-28 17:34:08 +0100

    Added a SummonerEntity class to store summoner data in the db and added EntityFromRiotDto class to transfrom riot dto's to entities.

---
+++ LoLCrawler/DatabaseEntity/SummonerEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoLCrawler.DatabaseEntity
+{
+    public class SummonerEntity
+    {
+        public int SummonerEntityId { get; set; }
+        
+        //this is the value needed to access league position data.
+        public string SummonerId { get; set; }
+        //this is the id needed to access matchHistory data.
+        public string AccountId { get; set; }
+        public string Name { get; set; }
+        public string ProfileIconId { get; set; }
+        //milliseconds since epoch
+        public long RevisionDate { get; set; }
+        public int SummonerLevel { get; set; }
+    }
+}
---
+++ LoLCrawler/EntityFromRiotDto.cs
@@ -0,0 +1,23 @@
+﻿using LoLCrawler.DatabaseEntity;
+using RiotData.LoLCrawler;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoLCrawler
+{
+    public static class EntityFromRiotDto
+    {
+        public static SummonerEntity GetSummoner(Summoner summoner)
+        {
+            SummonerEntity result = new SummonerEntity();
+            result.AccountId = summoner.accountId;
+            result.Name = summoner.name;
+            result.ProfileIconId = summoner.profileIconId;
+            result.RevisionDate = summoner.revisionDate;
+            result.SummonerId = summoner.id;
+            result.SummonerLevel = summoner.summonerLevel;
+            return result;
+        }
+    }
+}

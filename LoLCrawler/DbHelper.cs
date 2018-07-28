1d3f33919eb07bffc061a019452a95dd1fc678a9
James Hobday <jameshobday84@gmail.com>
2018-07-28 17:54:42 +0100

    Added DbHelper methdos for checking if a summonerEntity does not already exists and adding it if it doesn't.

--- LoLCrawler/DbHelper.cs
+++ LoLCrawler/DbHelper.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using LoLCrawler.DatabaseEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace LoLCrawler
 {
-    class DbHelper
+    public class DbHelper
     {
-        
+        NamesDbContext context = new NamesDbContext();
+        //rteurns true if already exists.
+        private bool summonerAlreadyExists(SummonerEntity summoner)
+        {
+            var queryResult = context.Summoners.Where(x => x.Name == summoner.Name).SingleOrDefault();
+            return (queryResult != null);
+        }
+        public bool SubmitSummonerIfNotDuplicate(SummonerEntity summoner)
+        {
+            if (!summonerAlreadyExists(summoner))
+            {
+                context.Add(summoner);
+                return true;
+            }
+            return false;
+        }
     }
 }

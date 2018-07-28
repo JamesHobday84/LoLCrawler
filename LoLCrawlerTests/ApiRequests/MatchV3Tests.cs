62b6525973a6dc4b314a0c8cf52e6be0b55bb5a8
James Hobday <jameshobday84@gmail.com>
2018-07-28 01:19:28 +0100

    Added tests for matchV3Requests.

---
+++ LoLCrawlerTests/ApiRequests/MatchV3Tests.cs
@@ -0,0 +1,54 @@
+﻿using LoLCrawler;
+using LoLCrawler.ApiRequests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LoLCrawlerTests.ApiRequests
+{
+    public class MatchV3RequestTests
+    {
+        [Fact]
+        public void MatchReturnsNullIfInvalidId()
+        {
+            var sut = new MatchV3Request(new RequestStringHolder());
+            var matchId = "10";
+
+            var result = sut.Match(matchId);
+
+            Assert.Null(result);
+        }
+        [Fact]
+        public void MatchReturnsMatchIfIdValid()
+        {
+            var sut = new MatchV3Request(new RequestStringHolder());
+            var matchId = "3715147769";//valid matchId;
+
+            var result = sut.Match(matchId);
+
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void MatchListReturnsNullIfIdInvalid()
+        {
+            var sut = new MatchV3Request(new RequestStringHolder());
+            var accountId = "10";
+
+            var result = sut.Match(accountId);
+
+            Assert.Null(result);
+        }
+        [Fact]
+        public void MatchListReturnsMatchListIfIdValid()
+        {
+            var sut = new MatchV3Request(new RequestStringHolder());
+            var accountId = "24162553";//Valid accountId;
+
+            var result = sut.MatchList(accountId);
+
+            Assert.NotNull(result);
+        }
+        
+    }
+}

94fb0ff93fc71be42df54f2afe5597e21612081e
James Hobday <jameshobday84@gmail.com>
2018-07-28 15:45:37 +0100

    Removed calls to deleted code in crawler from main.

--- LoLCrawler/Program.cs
+++ LoLCrawler/Program.cs
@@ -9,12 +9,7 @@ namespace LoLCrawler
     {
         static void Main(string[] args)
         {
-            Summoner summoner = new ApiRequest(new RequestStringHolder()).GetSummonerByName("unkownEntity");
-            DbDataStatsGather statsGatherer = new DbDataStatsGather();
-            statsGatherer.getLeagueDivStats();
-            Crawler crawler = new Crawler();
-            crawler.CollectNamesFromMatchHistory(1);
-            Console.ReadLine();
+            
         }
     }
 }

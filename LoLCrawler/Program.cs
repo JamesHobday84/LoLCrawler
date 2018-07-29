f5532177ae8ee56e7e64151d84dfc1b21b4a38d8
James Hobday <jameshobday84@gmail.com>
2018-07-29 01:41:43 +0100

    Added tempoery method in main to print out all the leagues currently in the db.

--- LoLCrawler/Program.cs
+++ LoLCrawler/Program.cs
@@ -1,7 +1,9 @@
-﻿using LoLCrawler.RiotData;
+﻿using LoLCrawler.DatabaseEntity;
+using LoLCrawler.RiotData;
 using RiotData.LoLCrawler;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LoLCrawler
 {
@@ -9,6 +11,13 @@ namespace LoLCrawler
     {
         static void Main(string[] args)
         {
+            //print out all laegue names.
+            NamesDbContext context = new NamesDbContext();
+            IEnumerable<LeagueEntity> leagues = context.Leagues.OrderBy(x => x.Tier);
+            foreach(LeagueEntity league in leagues)
+            {
+                Console.WriteLine($"{league.Name} : {league.Tier}");
+            }
             Crawler crawler = new Crawler();
             crawler.CollectLeagueNames();
             Console.ReadLine();
